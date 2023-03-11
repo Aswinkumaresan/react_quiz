74130fc30358bc19103874b6d15e48ae07ec2dfb
Aswinkumaresan <aswinabi2000@gmail.com>
2023-03-11 10:20:27 +0530

    task update

---
+++ quiz_3_identify.js
@@ -0,0 +1,13 @@
+//3)Identify all of the variables, object property names, primitive values, and objects shown in the following code
+
+function hello(greeting, name) {
+  return greeting + ' ' + name;
+}
+
+function xyzzy() {
+  return { a: 1, b: 2, c: [3, 4, 5], d: {} };
+}
+
+const howdy = hello('Hi', 'Grace');
+let foo = xyzzy();
+
